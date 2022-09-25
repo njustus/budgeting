d828a949c9f7390193496482e28f9688b2221f58
N. Justus <n-justus@outlook.com>
2022-09-25 18:07:05 +0200

    include font-awesome

--- src/components/transaction-list.vue
+++ src/components/transaction-list.vue
@@ -9,10 +9,16 @@ const transactions = transactionStore.transactions;
 </script>
 
 <template>
-<ul class="w3-ul">
-    <li v-for="(tx, idx) of transactions" v-bind:key="idx">
-        <p>{{tx.title}} ({{tx.amount}} €)</p>
-        <small>{{tx.description}}</small>
+<ul class="w3-ul w3-card-4">
+    <li class="w3-bar" v-for="(tx, idx) of transactions" v-bind:key="idx">
+        <span class="w3-bar-item w3-circle w3-left">
+            <i class="fa fa-home"></i>
+        </span>
+        <div class="w3-bar-item">
+            <span class="w3-large">{{tx.title}} ({{tx.amount}} €)</span>            
+            <small>{{tx.description}}</small>            
+        </div>
+        <strong class="w3-bar-item w3-right"> {{tx.amount}} € </strong>
     </li>
 </ul>
 </template>
