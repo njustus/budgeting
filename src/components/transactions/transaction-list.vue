3339d05a4c598ea1865ea91d3f2596b2153f72a1
N. Justus <n-justus@outlook.com>
2022-11-09 19:17:44 +0100

    display transactions as list

--- src/components/transactions/transaction-list.vue
+++ src/components/transactions/transaction-list.vue
@@ -1,14 +1,62 @@
 <script setup lang="ts">
-import { useAppStore } from '@/stores/app-state'
-import { inject } from 'vue'
+import {useAppStore} from '@/stores/app-state'
+import type {Transaction} from "@/models/state";
+import {TransactionType} from "@/models/state";
 
 const store = useAppStore();
-
-//TODO add styling for a list
 //TODO intermediate sums per month
+
+function monthDescription(idx:number): string | null {
+    if(idx===0) {
+        return null
+    }
+
+    const t1 = store.sortedTransactions[idx-1]
+    const t2 = store.sortedTransactions[idx]
+
+    const isMonthDifferent = t1.date.getFullYear() != t2.date.getFullYear() || t1.date.getMonth() != t2.date.getMonth()
+    return isMonthDifferent ? new Date(t2.date.getFullYear(), t2.date.getMonth(), 1).toLocaleDateString() : null
+}
+
+
+function getType(transaction: Transaction): 'error' | 'success' {
+  switch(transaction.type) {
+    case TransactionType.Expense:
+      return 'error'
+    case TransactionType.Income:
+      return 'success'
+  }
+}
 </script>
 
 <template>
+  <n-list>
+    <template v-for="(transaction, idx) of store.sortedTransactions" v-bind:key="idx">
+      <n-list-item v-if="monthDescription(idx)">
+        <n-thing>
+          <strong>{{ monthDescription(idx) }}</strong>
+          TODO display running sum
+        </n-thing>
+      </n-list-item>
+
+      <n-list-item>
+        <n-thing>
+          <template #description>
+            <n-tag type="info" size="small" :bordered="false" v-for="tag of transaction.tags">{{tag.name}}</n-tag>
+          </template>
+          {{transaction.title}}
+        </n-thing>
+
+        <template #prefix>{{transaction.date.toLocaleDateString()}}<small>&nbsp;({{transaction.recurrence}})</small></template>
+        <template #suffix>
+          <strong>
+            <n-text :type="getType(transaction)">{{transaction.amount.toLocaleString()}} €</n-text>
+          </strong>
+        </template>
+      </n-list-item>
+      </template>
+  </n-list>
+
   <ul>
     <li v-for="transaction of store.sortedTransactions">
       {{JSON.stringify(transaction)}}
