8eef5cf22e8101a66e01f7e794e1b39f3a94454f
N. Justus <n-justus@outlook.com>
2022-12-12 19:12:33 +0100

    ignore income-expense

--- src/views/TransactionEvaluationView.vue
+++ src/views/TransactionEvaluationView.vue
@@ -1,16 +1,13 @@
 <template>
 <div>
-  <income-expense></income-expense>
   <expense-per-month></expense-per-month>
   <expense-categories></expense-categories>
 </div>
 </template>
 
 <script setup lang="ts">
-import IncomeExpense from '@/components/evaluations/income-expense.vue'
 import ExpenseCategories from "@/components/evaluations/expense-categories.vue";
-import ExpensePerMonth from "@/components/evaluations/expense-per-month.vue";
-</script>
+import ExpensePerMonth from "@/components/evaluations/expense-per-month.vue";</script>
 
 <style scoped>
 
