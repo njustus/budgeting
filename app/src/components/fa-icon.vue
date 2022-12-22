2df1f34abd8ff1a04ec632d696ce2de2458a54c9
N. Justus <n-justus@outlook.com>
2022-12-22 14:50:29 +0100

    define custom icon component

---
+++ app/src/components/fa-icon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import {defineProps} from 'vue'
+
+interface Props {
+  type?: string,
+  icon: string
+}
+
+const props =defineProps<Props>()
+
+const cssClasses = ['fa', 'fa-'+(props.type || 'solid'), 'fa-'+props.icon]
+</script>
+
+<template>
+  <i :class="cssClasses"></i>
+</template>
+
+<style scoped>
+
+</style>
