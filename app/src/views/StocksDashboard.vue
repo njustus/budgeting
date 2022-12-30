a8f30a3d3d9400397603bf004fb21aee3cd4107d
N. Justus <n-justus@outlook.com>
2022-12-30 17:35:23 +0100

    display depotBalance in depot-listing

--- app/src/views/StocksDashboard.vue
+++ app/src/views/StocksDashboard.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import StockList from "@/components/stocks/stock-list.vue";
+import {currency} from '@/utils/formats'
+import {useAppStore} from '@/stores/app-state'
+
+const store = useAppStore();
+
 </script>
 
 <template>
-  <h1>Depot</h1>
+  <n-space :justify="'space-between'">
+    <h1>Depot</h1>
+    <h1>
+      <n-text :type="'success'">{{ currency.format(store.depotBalance) }}</n-text>
+    </h1>
+  </n-space>
   <stock-list></stock-list>
 </template>
 
