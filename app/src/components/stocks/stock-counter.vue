3bf154da48dce4b1a29a928ae4e3575a106ea301
N. Justus <n-justus@outlook.com>
2023-01-03 14:47:42 +0100

    edit stock count

---
+++ app/src/components/stocks/stock-counter.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type {SubscribedStock} from "@/models";
+import {currency} from '@/utils/formats'
+import {ref} from "vue";
+import FaIcon from "@/components/common/fa-icon.vue";
+
+interface Props {
+  stock: SubscribedStock
+}
+
+const props = defineProps<Props>()
+
+const editableStock$ = ref<SubscribedStock | null>(null)
+
+function toggleEditableStock(stock: SubscribedStock) {
+  editableStock$.value = stock
+}
+
+</script>
+
+<template>
+  <template v-if="stock.id !== editableStock$?.id">
+    {{ stock.count }} x {{ currency.format(stock.stockInfo.exchange.price) }}
+    <n-button :type="'warning'" @click="toggleEditableStock(stock)">
+      <fa-icon icon="pen"></fa-icon>
+    </n-button>
+  </template>
+
+  <template v-if="stock.id === editableStock$?.id">
+    <n-input-number v-model:value="stock.count" @focusout="toggleEditableStock(null)"></n-input-number>
+  </template>
+</template>
+
+<style scoped>
+
+</style>
--- app/src/components/stocks/stock-list.vue
+++ app/src/components/stocks/stock-list.vue
@@ -4,9 +4,14 @@ import {currency} from '@/utils/formats'
 import type {SubscribedStock} from "@/models";
 import {stocksConfig} from '@/config'
 import StockForm from "@/components/stocks/stock-form.vue";
+import {computed} from "vue";
+import FaIcon from "@/components/common/fa-icon.vue";
+import StockCounter from "@/components/stocks/stock-counter.vue";
 
 const store = useAppStore();
-const subscribedStocks = store.depot.subscribedStocks
+const subscribedStocks = computed(() => store.depot.subscribedStocks)
+
+const deleteStockById = store.deleteStockById
 
 function total(stock: SubscribedStock): number {
   return stock.count * stock.stockInfo.exchange.price
@@ -20,8 +25,9 @@ function total(stock: SubscribedStock): number {
     <n-list-item v-for="stock of subscribedStocks" v-bind:key="stock.isin">
       <n-grid :cols="6" #default>
         <n-gi>
-          <small>{{ stock.count }} x {{ currency.format(stock.stockInfo.exchange.price) }}</small>
+          <stock-counter :stock="stock"></stock-counter>
         </n-gi>
+
         <n-gi :span="5">
           <a :href="stocksConfig.formatStocksUrl(stock.isin)" target="_blank">{{ stock.stockInfo.name }} ({{ stock.stockInfo.wkn }})</a><br/>
           <small>{{ stock.isin }}</small>
@@ -30,6 +36,7 @@ function total(stock: SubscribedStock): number {
 
       <template #suffix>
         <n-text :type="'info'"><strong>{{ currency.format(total(stock)) }}</strong></n-text>
+        <n-button :type="'error'" @click="deleteStockById(stock.id)"><fa-icon icon="trash"></fa-icon></n-button>
       </template>
     </n-list-item>
   </n-list>
