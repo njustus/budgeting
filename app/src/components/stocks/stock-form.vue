b548c76a946f3eba3512e0bdafc4f1338eaffce1
N. Justus <n-justus@outlook.com>
2023-07-15 16:36:09 +0200

    use autocomplete when searching for stocks

--- app/src/components/stocks/stock-form.vue
+++ app/src/components/stocks/stock-form.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import {useAppStore} from '@/stores/app-state'
-import {inject, ref, h, computed} from "vue";
+import {inject, ref, h, computed, watchEffect, reactive} from "vue";
 import {StocksClient} from "@/services";
 import {useNotification, NButton} from "naive-ui";
 import type {StockInfo} from "@/models";
@@ -15,11 +15,25 @@ const stockRef$ = ref({
 })
 
 const stockDetails$ = ref<StockInfo|null>(null)
+const stockOptions$ = ref<StockInfo[]>([]);
+const isLoading$ = ref<boolean>(false)
+
+const autoCompleteOptions$ = computed(() =>
+   stockOptions$.value.map(stock => ({label: `${stock.name} ${stock.isin} (${stock.wkn})`, value: stock.isin}))
+)
+
+watchEffect(async () => {
+  stockOptions$.value = await findStock(stockRef$.value.isin);
+});
 
 const amount$ = computed(() =>
   (stockDetails$.value) ?
     stockRef$.value.count * stockDetails$.value.exchange.price : 0.0
 )
+
+function updateStockDetails(selectedIsin: string) {
+  stockDetails$.value = stockOptions$.value.find(stock => stock.isin === selectedIsin) || null
+}
 
 function saveStock() {
   if(!stockDetails$.value) {
@@ -29,42 +43,15 @@ function saveStock() {
   appStore.addStock(stockDetails$.value, stockRef$.value.count)
 }
 
-async function findStock() {
-  if(stockRef$.value.isin.length<5) {
-    return
+async function findStock(isin?: string):Promise<StockInfo[]> {
+  if(!isin || isin.length<5) {
+    return []
   }
 
+  isLoading$.value = true
   const stockInfos = await stocksClient.findStockInfo(stockRef$.value.isin)
-
-  const title = 'Stock Search'
-  if(stockInfos.length<=0) {
-    notification.error({
-      title,
-      content: 'Unknown Stock: '+stockRef$.value.isin,
-    })
-  } else if(stockInfos.length>1) {
-    const stocksDescription = stockInfos.map(x => x.name).join(', ')
-    notification.warning({
-      title,
-      content: 'Multiple Stocks found: '+stocksDescription
-    });
-  } else {
-    stockDetails$.value = stockInfos[0]
-    const stockDescription = `Name: ${stockDetails$.value.name} - WKN: ${stockDetails$.value.wkn} - ISIN: ${stockDetails$.value.isin}`
-
-    const n = notification.success({
-      title,
-      content: 'Found stock '+stockDescription,
-      action: () => h(NButton, {
-        onClick: () => {
-          n.destroy()
-          saveStock()
-        }
-        },
-        {default: () => 'Save'}
-      )
-    })
-  }
+  isLoading$.value = false
+  return stockInfos
 }
 
 </script>
@@ -80,7 +67,19 @@ async function findStock() {
 
     <n-gi :span="4">
       <n-form-item>
-        <n-input v-model:value="stockRef$.isin" @focusout="findStock" placeholder="ISIN/WKN"></n-input>
+      <n-auto-complete
+        v-model:value="stockRef$.isin"
+        :input-props="{ autocomplete: 'disabled' }"
+        :options="autoCompleteOptions$"
+        :loading="isLoading$"
+        placeholder="Type a ISIN/WKN"
+        @select="updateStockDetails"
+      ></n-auto-complete>
+      </n-form-item>
+    </n-gi>
+    <n-gi>
+      <n-form-item>
+        <n-button :type="'primary'" @click="saveStock">Save</n-button>
       </n-form-item>
     </n-gi>
   </n-grid>
