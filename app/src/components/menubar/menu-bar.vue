4f2a9519d64d5472afa581aa27038ead4bc57fcf
N. Justus <n-justus@outlook.com>
2023-07-16 18:26:11 +0200

    restructure mobile menu

--- app/src/components/menubar/menu-bar.vue
+++ app/src/components/menubar/menu-bar.vue
@@ -8,7 +8,7 @@ import FaIcon from '@/components/common/fa-icon.vue'
 import {pageSize} from "@/services";
 
 const pageDimensions = pageSize.setup()
-const collapsed = ref(false)
+const showDrawer = ref(false)
 
 const menuOptions: MenuOption[] = [
   {
@@ -38,17 +38,29 @@ const menuOptions: MenuOption[] = [
       v-if="!pageDimensions.isMobile" />
 
     <!-- menu for mobiles -->
-    <n-popover
-      trigger="click"
-      v-if="pageDimensions.isMobile">
-      <template #trigger>
-        <n-button quaternary><FaIcon icon="bars"></FaIcon></n-button>
+    <n-button @click="() => showDrawer=true" v-if="pageDimensions.isMobile">
+      <FaIcon icon="bars"></FaIcon>
+    </n-button>
+    <n-drawer v-model:show="showDrawer" width="70%" placement="left">
+      <n-drawer-content title="Menu">
+        <n-menu mode="vertical" :options="menuOptions"></n-menu>
+
+      <template #footer>
+        <n-grid :cols="1">
+          <n-gi>
+            <SyncButton :sizing="pageDimensions.componentSizing"></SyncButton>
+          </n-gi>
+          <n-gi>
+            <ConnectionIndicator :sizing="pageDimensions.componentSizing"></ConnectionIndicator>
+          </n-gi>
+        </n-grid>
       </template>
-      <n-menu mode="vertical" :options="menuOptions"></n-menu>
-    </n-popover>
+
+      </n-drawer-content>
+    </n-drawer>
 
     <!-- displayed everywhere -->
-    <SyncButton :sizing="pageDimensions.componentSizing"></SyncButton>
+    <SyncButton :sizing="pageDimensions.componentSizing" v-if="!pageDimensions.isMobile"></SyncButton>
     <ConnectionIndicator :sizing="pageDimensions.componentSizing"></ConnectionIndicator>
   </n-space>
 
